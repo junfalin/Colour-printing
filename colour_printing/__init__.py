1d007cc12f94245d096716f64124fc3912fb4d29
faithforus <ljunf817@163.com>
2019-08-11 21:30:40 +0800

    optimization

--- colour_printing/__init__.py
+++ colour_printing/__init__.py
@@ -1,1 +1,2 @@
 from .log import ColourPrint, Switch
+
--- colour_printing/log.py
+++ colour_printing/log.py
@@ -8,19 +8,47 @@ class Switch:
 
 
 class ColourPrint:
+    """
+        @format：
+                【category】time string
+        @usage:
+            default: INFO, ERROR, WARRING, SUCCESS
+                >> log=ColourPrint()
+
+                >> log('hello')
+
+                >> [INFO] 2019-08-11 17:38:20 hello
+
+                >> log('hello',category='ERROR')
+
+                >> [ERROR] 2019-08-11 17:53:27 hello
+
+            user: category_name
+                >> log=ColourPrint()
+
+                >> log.set_str_style(category=category_name,mode='',fore='',back='')
+
+                >> log.set_time_style(...)
+
+                >> log.set_category_style(...)
+
+        @help:
+                >> print(ColourPrint())
+
+        @attr:
+                self.config={
+                            category:{
+                                    'str_style':(style,end),
+                                    'category_style':(style,end),
+                                    'time_style':(style,end),
+                            }
+                }
+    """
+
     time = lambda _: datetime.strftime(datetime.now(), '%Y-%m-%d %H:%M:%S')
     template = '{category_style0}[{category}]{category_style1} {time_style0}{time}{time_style1} {str_style0}{string}{str_style1}'
 
     def __init__(self):
-        """
-        self.config={
-                    category:{
-                            'str_style':(style,end),
-                            'category_style':(style,end),
-                            'time_style':(style,end),
-                    }
-        }
-        """
         self.switch = Switch
         self.config = {}
         self.__default_setting()
@@ -74,7 +102,7 @@ class ColourPrint:
     def __user_setting(self, category):
         style = self.config.get(category)
         if not style:
-            raise KeyError('还未设定此类型，请set_str_style,set_time_style')
+            raise KeyError(f'还未设定>{category}<类型，请set_str_style,set_time_style...')
         str_style = style.pop(STR_STYLE, self.__setting())
         time_style = style.pop(TIME_STYLE, self.__setting())
         category_style = style.pop(CATEGORY_STYLE, self.__setting())
@@ -93,3 +121,41 @@ class ColourPrint:
             return
         template = self.__user_setting(category)
         print(template.format(data))
+
+    def __str__(self):
+        return """{
+            'fore':
+                {  # 前景色
+                    'black': 30,  # 黑色
+                    'red': 31,  # 红色
+                    'green': 32,  # 绿色
+                    'yellow': 33,  # 黄色
+                    'blue': 34,  # 蓝色
+                    'purple': 35,  # 紫红色
+                    'cyan': 36,  # 青蓝色
+                    'white': 37,  # 白色
+                },
+
+            'back':
+                {  # 背景
+                    'black': 40,  # 黑色
+                    'red': 41,  # 红色
+                    'green': 42,  # 绿色
+                    'yellow': 43,  # 黄色
+                    'blue': 44,  # 蓝色
+                    'purple': 45,  # 紫红色
+                    'cyan': 46,  # 青蓝色
+                    'white': 47,  # 白色
+                },
+
+            'mode':
+                {  # 显示模式
+                    'mormal': 0,  # 终端默认设置
+                    'bold': 1,  # 高亮显示
+                    'underline': 4,  # 使用下划线
+                    'blink': 5,  # 闪烁
+                    'invert': 7,  # 反白显示
+                    'hide': 8,  # 不可见
+                },
+
+        }"""
