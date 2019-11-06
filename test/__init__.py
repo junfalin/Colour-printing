8c3246e5285bb3775ca95acf3e43101f9bf7b668
faithforus <ljunf817@163.com>
2019-11-06 13:46:43 +0800

    fix

--- test/__init__.py
+++ test/__init__.py
@@ -1,12 +1,13 @@
-from colour_printing.default import log
+# from colour_printing.default import log
 from colour_printing.custom import PrintMe
+from test.colour_printing_config import CP
 
-PrintMe.DYE = False
-# log.info('hello')
-# log.success('hello', flag='', level='ss')
-# log.error('hello')
-# log.warning('hello')
-# log.debug('hello')
-from colour_printing import cprint, Fore
+if __name__ == '__main__':
 
-cprint('sd', Fore.BLUE)
+    log = PrintMe(CP)
+    log.info('hello')
+    log.success('hello', flag='', level='ss')
+    log.error('hello')
+    log.warning('hello')
+    log.debug('hello')
+
